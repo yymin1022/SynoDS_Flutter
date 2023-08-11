08c7b51f2a6c8fe0779acbef2aba47d1c7993f1d
yymin1022 <yymin1022@gmail.com>
2023-08-11 21:10:40 +0900

    Update MainPage : Added Basic Element

---
+++ lib/pages/MainPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return(
+      const Scaffold(
+        body: (
+          SafeArea(
+            child: (
+              Text("Main Page")
+            )
+          )
+        )
+      )
+    );
+  }
+}
