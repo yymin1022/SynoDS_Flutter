dbb76f1dd8692ea6f86034f5b46198744bd18ef8
yymin1022 <yymin1022@gmail.com>
2023-08-11 20:59:32 +0900

    Update LoginPage : Added Basic Element

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return(
       const Scaffold(
         body: (
-          Text("Login Page")
+          SafeArea(
+            child: (
+              Text("Login Page")
+            )
+          )
         )
       )
     );
