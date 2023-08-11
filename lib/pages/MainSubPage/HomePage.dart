23a6b230091aab35d526dfe10c8a895517dbb970
yymin1022 <yymin1022@gmail.com>
2023-08-11 21:51:09 +0900

    Added HomePage

---
+++ lib/pages/MainSubPage/HomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return(
+      const Scaffold(
+        body: (
+          SafeArea(
+            child: (
+              Text("Home Page")
+            )
+          )
+        )
+      )
+    );
+  }
+}
