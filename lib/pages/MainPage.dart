a6cd0695d47dfff653cb6b25425e41f19a475e8e
yymin1022 <yymin1022@gmail.com>
2023-08-12 00:12:11 +0900

    Update MainPage : Applied BottomNavigationBar

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:syno_ds/pages/MainSubPage/HomePage.dart';
+import 'package:syno_ds/pages/MainSubPage/SettingPage.dart';
+import 'package:syno_ds/pages/MainSubPage/TaskAddPage.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage> {
+  int _pageIdx = 1;
+
+  final List<Widget> _pageList = <Widget>[
+    const TaskAddPage(),
+    const HomePage(),
+    const SettingPage()
+  ];
+
+  void _onNavBarTapped(int idx) {
+    setState(() {
+      _pageIdx = idx;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return(
-      const Scaffold(
+      Scaffold(
         body: (
           SafeArea(
-            child: (
-              Text("Main Page")
-            )
+            child: _pageList[_pageIdx]
           )
-        )
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _pageIdx,
+          onTap: _onNavBarTapped,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: "Add Task"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Setting"
+            )
+          ]
+        ),
       )
     );
   }
