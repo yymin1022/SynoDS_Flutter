70730092b6b86656076f9fb9392757098219a2e1
yymin1022 <yymin1022@gmail.com>
2023-08-11 21:48:02 +0900

    Update LoginPage : Applied State Change of Main Login State

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:syno_ds/main.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final synoDSApp = context.findAncestorStateOfType<SynoDSAppState>();
+
     return(
-      const Scaffold(
+      Scaffold(
         body: (
           SafeArea(
             child: (
-              Text("Login Page")
+              Column(
+                children: [
+                  const Text("Login Page"),
+                  ElevatedButton(
+                    onPressed: (){tryLogin(synoDSApp);},
+                    child: const Text("Login")
+                  )
+                ],
+              )
             )
           )
         )
       )
     );
+  }
+
+  void tryLogin(synoDSApp) {
+    synoDSApp.setState(() {
+      synoDSApp.isLoggedIn = true;
+    });
   }
 }
