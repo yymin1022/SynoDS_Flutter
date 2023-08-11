ca5822b2473a2b7ad7deacdb4b9406ae33b97b0b
yymin1022 <yymin1022@gmail.com>
2023-08-11 21:52:53 +0900

    Added SettingPage

---
+++ lib/pages/MainSubPage/SettingPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return(
+      const Scaffold(
+        body: (
+          SafeArea(
+            child: (
+              Text("Setting Page")
+            )
+          )
+        )
+      )
+    );
+  }
+}
